15030e9d74d4e7ab66ee918ab211cabf01910fb7
Miguel Galicia <migel.gt@outlook.com>
2023-02-09 20:41:32 -0600

    Add Swap shaker implementation

---
+++ vns/shaking/swap.go
@@ -0,0 +1,35 @@
+package shaking
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/mig-elgt/tsp/vns"
+)
+
+type swap struct{}
+
+// NewSwap creates new swap shaker
+func NewSwap() *swap {
+	return &swap{}
+}
+
+// Shake performs a swap operation between two random stops(customers)
+// and creates a new solution as result after the operation.
+func (swap) Shake(so *vns.Solution, res *vns.Solution) {
+	clusterCopy := make([]int, len(so.Customers))
+	copy(clusterCopy, so.Customers)
+	res = &vns.Solution{
+		Cluster:   so.Cluster,
+		Customers: clusterCopy,
+		Routes:    so.Routes,
+	}
+	i := randInt(0, len(so.Customers))
+	j := randInt(0, len(so.Customers))
+	res.Customers[i], res.Customers[j] = res.Customers[j], res.Customers[i]
+}
+
+func randInt(min int, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min+1) + min
+}
