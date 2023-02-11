fc0dd89009eba6b7b08e6ffe3837e7cdb7ec460e
Miguel Galicia <migel.gt@outlook.com>
2023-02-11 11:09:38 -0600

    Add domain model data to describe a Table Service

---
+++ table/table.go
@@ -0,0 +1,21 @@
+package table
+
+// Locations represents a destination location
+type Location struct {
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
+}
+
+// Cost describes the costs between two locations
+type Cost struct {
+	// Duration between two locations in seconds
+	Duration float64 `json:"duration"`
+	// Distance between two locations in meters
+	Distance float64 `json:"distance"`
+}
+
+// TableService describes the behavior to performs a Distance Matrix object
+type TableService interface {
+	// Fetch fetches a cost distance matrix for a set of locations
+	Fetch(locations []*Location) ([][]*Cost, error)
+}
